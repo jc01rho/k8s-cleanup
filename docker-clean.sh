78c301df673ffe90f46746f13872879b96085675
jc01rho <rkjnice@gmail.com>
2022-06-28 13:28:53 +0900

    Update docker-clean.sh

--- docker-clean.sh
+++ docker-clean.sh
@@ -4,9 +4,19 @@ while true; do
   # Remove exited containers
   docker ps -a -q -f status=exited    | xargs --no-run-if-empty docker rm -v
   # Remove dangling images
-  docker images -f "dangling=true" -q | xargs --no-run-if-empty docker rmi
+  #docker images -f "dangling=true" -q | xargs --no-run-if-empty docker rmi
+  docker image prune --all --force
   # Remove dangling volumes
   docker volume ls -qf dangling=true  | xargs --no-run-if-empty docker volume rm
+  
+  
+  
+  if ! [ -x "$(command -v crictl)" ]; then
+  echo 'Error: git is not installed.' >&2
+  else
+  crictl rmi --prune
+  fi
+
 
   # DOCKER_CLEAN_INTERVAL defaults to 30min
   sleep $DOCKER_CLEAN_INTERVAL
