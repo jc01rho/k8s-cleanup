dd85b8fe491d019904086cbf9fd8f6f6b98cab81
whrho <whrho@sparrowfasoo.com>
2023-05-04 15:38:16 +0900

    fixed

--- vm-cache-clean.sh
+++ vm-cache-clean.sh
@@ -4,6 +4,7 @@ while true; do
   sync
   echo 1 > /proc/sys/vm/compact_memory
   echo 3 > /proc/sys/vm/drop_caches
+  sync
 
   # CONATINERD_CLEAN_INTERVAL defaults to 30min
   sleep $DROP_CACHES_INTERVAL
