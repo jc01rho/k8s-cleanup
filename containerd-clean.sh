164cd1c98dbdb13723980a0f42a18b16f87003a1
whrho <whrho@sparrowfasoo.com>
2023-05-04 14:18:14 +0900

    fixed ymls

--- containerd-clean.sh
+++ containerd-clean.sh
@@ -5,6 +5,7 @@ while true; do
   echo 'Error: crictl is not installed.' >&2
   else
   crictl rmi --prune
+  crictl  ps --all | grep Exited | awk '{print $1}' | xargs -L1 crictl rm
   fi
 
   # CONATINERD_CLEAN_INTERVAL defaults to 30min
---
+++ vm-cache-clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do  
+  sync
+  echo 1 > /proc/sys/vm/compact_memory
+  echo 3 > /proc/sys/vm/drop_caches
+
+  # CONATINERD_CLEAN_INTERVAL defaults to 30min
+  sleep $DROP_CACHES_INTERVAL
+done
