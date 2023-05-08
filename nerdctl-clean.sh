40517ac9898ceffd08d7d5ca3e3f35a07d5b872f
whrho <whrho@sparrowfasoo.com>
2023-05-08 11:34:34 +0900

    added nerdctl

---
+++ nerdctl-clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true; do  
+  if ! [ -x "$(command -v nerdctl)" ]; then
+  echo 'Error: crictl is not installed.' >&2
+  else
+  nerdctl image prune --all --force
+  nerdctl container prune -f
+  nerdctl volume prune -f
+  nerdctl -n k8s.io image prune --all --force
+  nerdctl -n k8s.io container prune -f
+  nerdctl -n k8s.io volume prune -f
+  fi
+
+  # CONATINERD_CLEAN_INTERVAL defaults to 30min
+  sleep $CONATINERD_CLEAN_INTERVAL
+done
